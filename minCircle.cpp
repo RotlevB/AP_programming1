89adf0bfe727561df7998953bf4cf17e427ffd4c
RotlevB <73072826+RotlevB@users.noreply.github.com>
2021-12-15 18:08:24 +0200

    Update minCircle.cpp

--- minCircle.cpp
+++ minCircle.cpp
@@ -1,1 +1,13 @@
 #include "minCircle.h"
+
+
+
+Circle findMinCircle(Point** points, size_t size) {
+
+}
+Circle findMinCircle(unordered_set<Point*>& p, unordered_set<Point*>& r) {
+
+}
+Circle trivial(vector<Point*> points) {
+
+}
